e9e1bfc59c0c4cc01df529eacf8e01d36a9ab840
JavsRecTc02 <tenoriojavierc@gmail.com>
2024-10-21 14:00:37 -0600

    Decoder 4 bits - Registro - Testbench FUNCIONAL

--- FAC_P1/tb_top_module.sv
+++ FAC_P1/tb_top_module.sv
@@ -4,26 +4,32 @@
 // FUNCIONAMIENTO DE LAS BANDERAS ( Z, C, V, S )
 // VELOCIDADES PWM ( 25%, 50%, 75%, 100% )
 
+
 module tb_top_module;
 
     // Señales de entrada
     logic clk;
     logic rst;
-    logic [1:0] alu_a;
+    logic A, B, C, D;    // Entradas del decodificador (dedos)
     logic [1:0] alu_b;
     logic [1:0] alu_op;
-    
+
     // Señales de salida
     logic motor_pwm;
+    logic [6:0] seg1;
 
     // Instancia del top module
     top_module dut (
         .clk(clk),
         .rst(rst),
-        .alu_a(alu_a),
+        .A(A),
+        .B(B),
+        .C(C),
+        .D(D),
         .alu_b(alu_b),
         .alu_op(alu_op),
-        .motor_pwm(motor_pwm)
+        .motor_pwm(motor_pwm),
+        .seg1(seg1)
     );
 
     // Generación del reloj
@@ -34,7 +40,6 @@ module tb_top_module;
         // Inicialización de señales
         clk = 0;
         rst = 0;
-        alu_a = 2'b00;
         alu_b = 2'b00;
         alu_op = 2'b00;
 
@@ -42,63 +47,93 @@ module tb_top_module;
         #10 rst = 1;
         #10 rst = 0;
 
-        // Caso de prueba 1: Suma (00) con a = 2, b = 1
-        alu_a = 2'b10; 
+        // Combinaciones de prueba para el decodificador - ALU - PWM
+		  
+        // Entrada 1000
+        A = 1;
+        B = 0;
+        C = 0;
+        D = 0;
         alu_b = 2'b01; 
         alu_op = 2'b00; // Suma
         #20;
-        $display("ALU Operation: SUM, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a + alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
+        $display("Dec: 1000, Decoder Result: %b, ALU IN2: %b, ALU Operation: SUM, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", dut.dec_result, alu_b, dut.alu_result, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
+		//$display("ALU Operation: SUM, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a + alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
 
-        // Caso de prueba 2: Resta (01) con a = 3, b = 1
-        alu_a = 2'b11; 
-        alu_b = 2'b01; 
-        alu_op = 2'b01; // Resta
-        #20;
-        $display("ALU Operation: SUBTRACT, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a - alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
 
-        // Caso de prueba 3: AND (10) con a = 3, b = 2
-        alu_a = 2'b01; 
-        alu_b = 2'b10; 
-        alu_op = 2'b10; // AND
-        #20;
-        $display("ALU Operation: AND, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a & alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
-
-        // Caso de prueba 4: OR (11) con a = 1, b = 0
-        alu_a = 2'b01; 
-        alu_b = 2'b00; 
+        // Entrada 1100
+        A = 1;
+        B = 1;
+        C = 0;
+        D = 0;
+        alu_b = 2'b11;
         alu_op = 2'b11; // OR
         #20;
-        $display("ALU Operation: OR, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a | alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
+		  $display("Dec: 1100, Decoder Result: %b, ALU IN2: %b, ALU Operation: OR, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", dut.dec_result, alu_b, dut.alu_result, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
+		
 
-        // Casos para verificar las banderas:
+        // Entrada 1110
+        A = 1;
+        B = 1;
+        C = 1;
+        D = 0;
+        alu_b = 2'b10;
+        alu_op = 2'b01; // Resta
+        #20;
+		  $display("Dec: 1110, Decoder Result: %b, ALU IN2: %b, ALU Operation: SUB, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", dut.dec_result, alu_b, dut.alu_result, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
+		
+    
+        // Entrada 1111
+        A = 1;
+        B = 1;
+        C = 1;
+        D = 1;
+        alu_b = 2'b10;
+        alu_op = 2'b10; // AND
+        #20;
+		  $display("Dec: 1111, Decoder Result: %b, ALU IN2: %b, ALU Operation: AND, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", dut.dec_result, alu_b, dut.alu_result, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
+		
+		  //Casos para verificar las banderas de la ALU:
 
         // Bandera Zero: Cuando a = 0, b = 0 en la operación AND (debe dar Z = 1)
-        alu_a = 2'b00; 
+		  A = 1;
+        B = 0;
+        C = 0;
+        D = 0;
         alu_b = 2'b00;
         alu_op = 2'b10; // AND
         #20;
-        $display("Flag Test - Z, ALU result: %b, Z: %b", alu_a & alu_b, dut.alu_inst.Z);
+        $display("Flag Test - Z, ALU result: %b, Z: %b", dut.alu_result, dut.alu_inst.Z);
 
         // Bandera Carry: Cuando a = 3, b = 3 en la operación SUM (debe dar C = 1)
-        alu_a = 2'b11; 
+        A = 1;
+        B = 1;
+        C = 1;
+        D = 1; 
         alu_b = 2'b11; 
         alu_op = 2'b00; // Suma
         #20;
-        $display("Flag Test - C, ALU result: %b, C: %b", alu_a + alu_b, dut.alu_inst.C);
+        $display("Flag Test - C, ALU result: %b, C: %b", dut.alu_result, dut.alu_inst.C);
 
         // Bandera Overflow: Cuando a = 2, b = 3 en la operación SUM (debe dar V = 1)
-        alu_a = 2'b10; 
+        A = 1;
+        B = 1;
+        C = 1;
+        D = 0; 
         alu_b = 2'b11; 
         alu_op = 2'b00; // Suma
         #20;
-        $display("Flag Test - V, ALU result: %b, V: %b", alu_a + alu_b, dut.alu_inst.V);
+        $display("Flag Test - V, ALU result: %b, V: %b", dut.alu_result, dut.alu_inst.V);
 
         // Bandera Signo: Cuando a = 1, b = 3 en la operación SUBTRACT (debe dar S = 1)
-        alu_a = 2'b01; 
+        A = 1;
+        B = 1;
+        C = 0;
+        D = 0; 
         alu_b = 2'b11; 
         alu_op = 2'b01; // Resta
         #20;
-        $display("Flag Test - S, ALU result: %b, S: %b", alu_a - alu_b, dut.alu_inst.S);
+        $display("Flag Test - S, ALU result: %b, S: %b", dut.alu_result, dut.alu_inst.S);
 
         // Finalizar simulación
         #100 $finish;
@@ -108,78 +143,108 @@ endmodule
 
 
 
-// PRUEBAS APARTE (NO TOMAR EN CUENTA)
 
-//initial begin
-//    // Inicialización de señales
-//    clk = 0;
-//    rst = 0;
-//    alu_a = 2'b00;
-//    alu_b = 2'b00;
-//    alu_op = 2'b00;
+
+//module tb_top_module;
 //
-//    // Reset del sistema
-//    #10 rst = 1;
-//    #10 rst = 0;
+//    // Señales de entrada
+//    logic clk;
+//    logic rst;
+//    logic [1:0] alu_a;
+//    logic [1:0] alu_b;
+//    logic [1:0] alu_op;
+//    
+//    // Señales de salida
+//    logic motor_pwm;
 //
-//    // Caso de prueba 1: Suma (00) con a = 2, b = 1
-//    alu_a = 2'b10; 
-//    alu_b = 2'b01; 
-//    alu_op = 2'b00; // Suma
-//    #20;
-//    $display("ALU Operation: SUM, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a + alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
+//    // Instancia del top module
+//    top_module dut (
+//        .clk(clk),
+//        .rst(rst),
+//        .alu_a(alu_a),
+//        .alu_b(alu_b),
+//        .alu_op(alu_op),
+//        .motor_pwm(motor_pwm)
+//    );
 //
-//    // Caso de prueba 2: Resta (01) con a = 3, b = 1
-//    alu_a = 2'b11; 
-//    alu_b = 2'b01; 
-//    alu_op = 2'b01; // Resta
-//    #20;
-//    $display("ALU Operation: SUBTRACT, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a - alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
+//    // Generación del reloj
+//    always #5 clk = ~clk; // Reloj con periodo de 10 unidades de tiempo
 //
-//    // Caso de prueba 3: AND (10) con a = 3, b = 2
-//    alu_a = 2'b11; 
-//    alu_b = 2'b10; 
-//    alu_op = 2'b10; // AND
-//    #20;
-//    $display("ALU Operation: AND, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a & alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
+//    // Testbench
+//    initial begin
+//        // Inicialización de señales
+//        clk = 0;
+//        rst = 0;
+//        alu_a = 2'b00;
+//        alu_b = 2'b00;
+//        alu_op = 2'b00;
 //
-//    // Caso de prueba 4: OR (11) con a = 1, b = 0
-//    alu_a = 2'b01; 
-//    alu_b = 2'b00; 
-//    alu_op = 2'b11; // OR
-//    #20;
-//    $display("ALU Operation: OR, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a | alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
+//        // Reset del sistema
+//        #10 rst = 1;
+//        #10 rst = 0;
 //
-//    // Casos para verificar las banderas:
+//        // Caso de prueba 1: Suma (00) con a = 2, b = 1
+//        alu_a = 2'b10; 
+//        alu_b = 2'b01; 
+//        alu_op = 2'b00; // Suma
+//        #20;
+//        $display("ALU Operation: SUM, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a + alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
 //
-//    // Bandera Zero: Cuando a = 0, b = 0 en la operación AND (debe dar Z = 1)
-//    alu_a = 2'b00; 
-//    alu_b = 2'b00;
-//    alu_op = 2'b10; // AND
-//    #20;
-//    $display("Flag Test - Z, ALU result: %b, Z: %b", alu_a & alu_b, dut.alu_inst.Z);
+//        // Caso de prueba 2: Resta (01) con a = 3, b = 1
+//        alu_a = 2'b11; 
+//        alu_b = 2'b01; 
+//        alu_op = 2'b01; // Resta
+//        #20;
+//        $display("ALU Operation: SUBTRACT, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a - alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
 //
-//    // Bandera Carry: Cuando a = 3, b = 3 en la operación SUM (debe dar C = 1)
-//    alu_a = 2'b11; 
-//    alu_b = 2'b11; 
-//    alu_op = 2'b00; // Suma
-//    #20;
-//    $display("Flag Test - C, ALU result: %b, C: %b", alu_a + alu_b, dut.alu_inst.C);
+//        // Caso de prueba 3: AND (10) con a = 3, b = 2
+//        alu_a = 2'b01; 
+//        alu_b = 2'b10; 
+//        alu_op = 2'b10; // AND
+//        #20;
+//        $display("ALU Operation: AND, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a & alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
 //
-//    // Bandera Overflow: Cuando a = 2, b = 3 en la operación SUM (debe dar V = 1)
-//    alu_a = 2'b10; 
-//    alu_b = 2'b11; 
-//    alu_op = 2'b00; // Suma
-//    #20;
-//    $display("Flag Test - V, ALU result: %b, V: %b", alu_a + alu_b, dut.alu_inst.V);
+//        // Caso de prueba 4: OR (11) con a = 1, b = 0
+//        alu_a = 2'b01; 
+//        alu_b = 2'b00; 
+//        alu_op = 2'b11; // OR
+//        #20;
+//        $display("ALU Operation: OR, ALU result: %b, Motor speed: %b, Flags - Z: %b, C: %b, V: %b, S: %b", alu_a | alu_b, dut.pwm_inst.motor_speed, dut.alu_inst.Z, dut.alu_inst.C, dut.alu_inst.V, dut.alu_inst.S);
 //
-//    // Bandera Signo: Cuando a = 1, b = 3 en la operación SUBTRACT (debe dar S = 1)
-//    alu_a = 2'b01; 
-//    alu_b = 2'b11; 
-//    alu_op = 2'b01; // Resta
-//    #20;
-//    $display("Flag Test - S, ALU result: %b, S: %b", alu_a - alu_b, dut.alu_inst.S);
+//        // Casos para verificar las banderas:
 //
-//    // Finalización
-//    $finish;
-//end
+//        // Bandera Zero: Cuando a = 0, b = 0 en la operación AND (debe dar Z = 1)
+//        alu_a = 2'b00; 
+//        alu_b = 2'b00;
+//        alu_op = 2'b10; // AND
+//        #20;
+//        $display("Flag Test - Z, ALU result: %b, Z: %b", alu_a & alu_b, dut.alu_inst.Z);
+//
+//        // Bandera Carry: Cuando a = 3, b = 3 en la operación SUM (debe dar C = 1)
+//        alu_a = 2'b11; 
+//        alu_b = 2'b11; 
+//        alu_op = 2'b00; // Suma
+//        #20;
+//        $display("Flag Test - C, ALU result: %b, C: %b", alu_a + alu_b, dut.alu_inst.C);
+//
+//        // Bandera Overflow: Cuando a = 2, b = 3 en la operación SUM (debe dar V = 1)
+//        alu_a = 2'b10; 
+//        alu_b = 2'b11; 
+//        alu_op = 2'b00; // Suma
+//        #20;
+//        $display("Flag Test - V, ALU result: %b, V: %b", alu_a + alu_b, dut.alu_inst.V);
+//
+//        // Bandera Signo: Cuando a = 1, b = 3 en la operación SUBTRACT (debe dar S = 1)
+//        alu_a = 2'b01; 
+//        alu_b = 2'b11; 
+//        alu_op = 2'b01; // Resta
+//        #20;
+//        $display("Flag Test - S, ALU result: %b, S: %b", alu_a - alu_b, dut.alu_inst.S);
+//
+//        // Finalizar simulación
+//        #100 $finish;
+//    end
+//
+//endmodule
+
+
--- FAC_P1/top_module.sv
+++ FAC_P1/top_module.sv
@@ -4,35 +4,100 @@
 module top_module(
     input logic clk,
     input logic rst,
-    input logic [1:0] alu_a,  // Entradas de la ALU
-    input logic [1:0] alu_b,
-    input logic [1:0] alu_op, // Selector de operación de la ALU
-	 output logic [6:0] seg1, // 7 segmentos para resultado en display
-    output logic motor_pwm    // Salida PWM para el motor
+    input logic A, B, C, D,    // Entradas para el decodificador (dedos)
+    input logic [1:0] alu_b,   // Entrada directa de la ALU
+    input logic [1:0] alu_op,  // Selector de operación de la ALU
+    output logic [6:0] seg1,   // Display de 7 segmentos para el resultado
+    output logic motor_pwm     // PWM para controlar el motor
 );
 
-    logic [1:0] alu_result;  // Resultado de la ALU
+    logic [1:0] dec_result;   // Resultado del decodificador
+    logic [1:0] alu_result;   // Resultado de la ALU
+    logic [1:0] reg_out;      // Salida del registro
+
+    // Instancia del Decodificador
+    FirtsDecoder_4to2bits decoder_inst (
+        .A(A),
+        .B(B),
+        .C(C),
+        .D(D),
+        .Y0(dec_result[1]),
+        .Y1(dec_result[0])
+    );
 
     // Instancia de la ALU
     alu alu_inst (
         .select(alu_op),
-        .a(alu_a),
+        .a(dec_result),
         .b(alu_b),
         .result(alu_result)
+    );
+	 
+	 // Registro para almacenar el resultado de la ALU
+    reg_2bit reg_inst (
+        .clk(clk),
+        .rst(rst),
+        .d(alu_result),     // Entrada del registro es el resultado de la ALU
+        .q(reg_out)         // Salida del registro
     );
 
     // Instancia del PWM, utilizando el resultado de la ALU como la velocidad
     pwm pwm_inst (
         .clk(clk),
         .rst(rst),
-        .speed(alu_result),   // El resultado de la ALU controla la velocidad del motor
+        .speed(reg_out),   // El resultado de la ALU y registro controla la velocidad del motor
         .motor_pwm(motor_pwm)
     );
 	 
 	 
 	  bin_to_7seg result_to_7seg ( //Mostrar el resultado
-        .bin(alu_result),
+        .bin(reg_out),
         .seg(seg1)
     );
 
 endmodule
+
+
+
+
+
+//// MODULO PRINCIPAL
+//
+//module top_module(
+//    input logic clk,
+//    input logic rst,
+//    input logic [1:0] alu_a,  // Entradas de la ALU
+//    input logic [1:0] alu_b,
+//    input logic [1:0] alu_op, // Selector de operación de la ALU
+//	 output logic [6:0] seg1, // 7 segmentos para resultado en display
+//    output logic motor_pwm    // Salida PWM para el motor
+//);
+//
+//    logic [1:0] alu_result;  // Resultado de la ALU
+//
+//    // Instancia de la ALU
+//    alu alu_inst (
+//        .select(alu_op),
+//        .a(alu_a),
+//        .b(alu_b),
+//        .result(alu_result)
+//    );
+//
+//    // Instancia del PWM, utilizando el resultado de la ALU como la velocidad
+//    pwm pwm_inst (
+//        .clk(clk),
+//        .rst(rst),
+//        .speed(alu_result),   // El resultado de la ALU controla la velocidad del motor
+//        .motor_pwm(motor_pwm)
+//    );
+//	 
+//	 
+//	  bin_to_7seg result_to_7seg ( //Mostrar el resultado
+//        .bin(alu_result),
+//        .seg(seg1)
+//    );
+//
+//endmodule
+
+
+
